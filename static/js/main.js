2e544af3a3de08c702cd6fc17c38f03ef3379f94
starkjoy <residualcode6@gmail.com>
2023-08-07 18:19:01 +0000

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const nameInput = document.getElementById("name-input");
     const nameDisplay = document.querySelector(".name");
 
-    nameInput.addEventListener("keypress", function(event) {
+    nameInput.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
             nameDisplay.textContent = nameInput.value;
         }
